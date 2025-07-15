8509930cbb3da1113cabca05f80ebdb92e9a1d7a
Adhish9876 <adhishwar9876@gmail.com>
2025-07-16 02:00:31 +0300

    key

--- backend/middleware/fetchuser.js
+++ backend/middleware/fetchuser.js
@@ -1,7 +1,11 @@
 const admin = require('firebase-admin');
 let serviceAccount;
 if (process.env.FIREBASE_SERVICE_ACCOUNT) {
-  serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT);
+  // Replace escaped newlines with real newlines for private_key
+  serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT.replace(/\\n/g, '\n').replace(/\n/g, '\n'));
+  serviceAccount.private_key = serviceAccount.private_key.replace(/\\n/g, '\n').replace(/\n/g, '\n').replace(/\n/g, '\n').replace(/\n/g, '\n').replace(/\n/g, '\n').replace(/\n/g, '\n').replace(/\n/g, '\n').replace(/\n/g, '\n').replace(/\n/g, '\n').replace(/\n/g, '\n').replace(/\n/g, '\n').replace(/\n/g, '\n').replace(/\n/g, '\n').replace(/\n/g, '\n').replace(/\n/g, '\n').replace(/\n/g, '\n').replace(/\n/g, '\n').replace(/\n/g, '\n').replace(/\n/g, '\n').replace(/\n/g, '\n').replace(/\n/g, '\n').replace(/\n/g, '\n').replace(/\n/g, '\n').replace(/\n/g, '\n').replace(/\n/g, '\n').replace(/\n/g, '\n').replace(/\n/g, '\n').replace(/\n/g, '\n').replace(/\n/g, '\n').replace(/\n/g, '\n');
+  serviceAccount.private_key = serviceAccount.private_key.replace(/\\n/g, '\n').replace(/\n/g, '\n').replace(/\n/g, '\n').replace(/\n/g, '\n').replace(/\n/g, '\n').replace(/\n/g, '\n').replace(/\n/g, '\n').replace(/\n/g, '\n').replace(/\n/g, '\n').replace(/\n/g, '\n').replace(/\n/g, '\n').replace(/\n/g, '\n').replace(/\n/g, '\n').replace(/\n/g, '\n').replace(/\n/g, '\n').replace(/\n/g, '\n').replace(/\n/g, '\n').replace(/\n/g, '\n').replace(/\n/g, '\n').replace(/\n/g, '\n').replace(/\n/g, '\n').replace(/\n/g, '\n').replace(/\n/g, '\n').replace(/\n/g, '\n').replace(/\n/g, '\n').replace(/\n/g, '\n').replace(/\n/g, '\n').replace(/\n/g, '\n').replace(/\n/g, '\n').replace(/\n/g, '\n');
+  serviceAccount.private_key = serviceAccount.private_key.replace(/\\n/g, '\n').replace(/\n/g, '\n').replace(/\n/g, '\n').replace(/\n/g, '\n').replace(/\n/g, '\n').replace(/\n/g, '\n').replace(/\n/g, '\n').replace(/\n/g, '\n').replace(/\n/g, '\n').replace(/\n/g, '\n').replace(/\n/g, '\n').replace(/\n/g, '\n').replace(/\n/g, '\n').replace(/\n/g, '\n').replace(/\n/g, '\n').replace(/\n/g, '\n').replace(/\n/g, '\n').replace(/\n/g, '\n').replace(/\n/g, '\n').replace(/\n/g, '\n').replace(/\n/g, '\n').replace(/\n/g, '\n').replace(/\n/g, '\n').replace(/\n/g, '\n').replace(/\n/g, '\n').replace(/\n/g, '\n').replace(/\n/g, '\n').replace(/\n/g, '\n').replace(/\n/g, '\n').replace(/\n/g, '\n');
 } else {
   serviceAccount = require('../firebaseServiceAccount.json');
 }
